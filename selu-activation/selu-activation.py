c1887126d712abf697fd01154c8fbb3c216d17f6
koctt <147679458+koctt-10@users.noreply.github.com>
2026-03-08 15:42:38 +0800

    Add solution for selu-activation

---
+++ selu-activation/selu-activation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def selu(x, lam=1.0507009873554804934193349852946, alpha=1.6732632423543772848170429916717):
+    """
+    Apply SELU activation element-wise.
+    Returns a list of floats rounded to 4 decimal places.
+    """
+    x = np.array(x)
+    result = []
+    for i in range(len(x)):
+        if x[i] > 0:
+            result.append(lam * x[i])
+        else:
+            result.append(lam *alpha * (np.exp(x[i]) - 1))
+    result = np.array(result)
+    return result
