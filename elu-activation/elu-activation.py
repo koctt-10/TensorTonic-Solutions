7044026612d4bbc3c213f4690672a203c9cf7a24
koctt <147679458+koctt-10@users.noreply.github.com>
2026-03-08 15:45:29 +0800

    Add solution for elu-activation

---
+++ elu-activation/elu-activation.py
@@ -0,0 +1,12 @@
+import math
+def elu(x, alpha):
+    """
+    Apply ELU activation to each element.
+    """
+    result = []
+    for i in range(len(x)):
+        if x[i] > 0:
+            result.append(x[i])
+        else:
+            result.append(alpha * (math.exp(x[i]) - 1))
+    return result
